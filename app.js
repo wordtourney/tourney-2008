a0cf64807b6e477866f57f484064764062b42507
wordtourney <jakegrantche@gmail.com>
2014-08-07 14:11:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ var main = function() {
     $('.dropdown-toggle').click(function() {
         $('.dropdown-menu').toggle();
     });
-    
-    $(function () { $('#collapseFour').collapse({
-      toggle: false
-   })});
-   $(function () { $('#collapseTwo').collapse('show')});
-   $(function () { $('#collapseThree').collapse('toggle')});
-   $(function () { $('#collapseOne').collapse('hide')});
 };
 
 $(document).ready(main);
